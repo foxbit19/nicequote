a520a58419fbb1c2eab147481d13562e5d0d9e01
foxbit19 <mattia.peretti@gmail.com>
2018-04-24 00:17:59 +0200

    Insert random font for each word

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 const fs = require('fs');
 var randomColor = require('random-color');
+const fontCount = 6;
 
 var router = express.Router();
 
@@ -14,9 +15,19 @@ function getRandomQuote(callback) {
 /* GET home page. */
 router.get('/', function (req, res, next) {
   getRandomQuote((quote) => {
-    var randomQuote = quote;
-    console.log(randomQuote);
-    console.log('Random quote: ' + randomQuote);
+    var randomQuote = '';
+    var randomSize;
+    var random;
+    var prevRandom;
+
+    quote.split(' ').forEach((quoteSplit) => {
+      while ((random = Math.floor((Math.random() * 100) % fontCount) + 1) == prevRandom);
+      prevRandom = random;
+      randomSize = (Math.floor(Math.random() * 100) % 4) + 6;
+      randomQuote += '<span class="font' + random + '" style="font-size: ' + randomSize + 'vh" >'
+      randomQuote += ' ' + quoteSplit + ' '
+      randomQuote += '</span>'
+    });
 
     res.render('index', {
       title: 'NiceQuote', quote: randomQuote, color: randomColor().hexString()
